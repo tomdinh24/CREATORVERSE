5dc31a8ab8422fb2e49bbcdab15d7cc91d737b15
Tom Lam <86568015+tomdinh24@users.noreply.github.com>
2023-08-06 15:27:44 -0700

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://nmerdefzqcxlajekrmem.supabase.co/";
+const URL = "URL";
 
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tZXJkZWZ6cWN4bGFqZWtybWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkyMDM5NTYsImV4cCI6MjAwNDc3OTk1Nn0.m9sBW7gvYnJGZl6akwUTRDQ_ssTvpMw-txLlSZOfLq4";
+const API_KEY = "API_Key";
 
 
 export const supabase = createClient( URL, API_KEY);
